35a955ebc701e76b51ef3538c54452cb79c42a52
davide bragagnolo <davidebraga.99@gmail.com>
2023-10-18 15:29:19 +0200

    add location

--- src/main/java/davidebragagnolo/Location.java
+++ src/main/java/davidebragagnolo/Location.java
@@ -1,4 +1,28 @@
 package davidebragagnolo;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "location")
+@Getter
+@Setter
+
 public class Location {
+    @Id
+    @GeneratedValue
+    private int id;
+    private String nome;
+    private String city;
+
+    public Location(String nome, String city) {
+        super();
+        this.nome = nome;
+        this.city = city;
+    }
 }
